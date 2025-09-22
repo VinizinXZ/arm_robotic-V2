7a665e49605b06a20569d4715b95fc417d8b3863
VinizinXZ <viniciosc141@gmail.com>
2025-09-22 16:32:40 -0300

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,18 @@
 #include <Servo.h>
 
 #define button1 A0
+#define button2 A1
 void setup() {
   Serial.begin(9600);
-  pinMode();
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+}
+void loop() {
+  int buttonState1 = analogRead(button1);
+  int buttonState2 = analogRead(button2);
+  Serial.print("Button 1: ");
+  Serial.print(buttonState1);
+  Serial.print(" - Button 2: ");
+  Serial.println(buttonState2);
+  delay(500);
 }
